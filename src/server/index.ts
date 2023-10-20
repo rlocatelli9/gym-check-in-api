2eec4dedbeb3e7e05bc3eefe486ac88a6fe6de1f
Robson Locatelli <rlocatelli9@gmail.com>
2023-10-20 09:01:16 -0300

    style: config eslint

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,11 @@
-import { env } from "env";
-import { app } from "src/app";
+import { env } from 'env'
+import { app } from 'src/app'
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT
-}).then(() =>{
-    console.log("server started on port: 3333")
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('server started on port: 3333')
+  })
