2604415da173b630ad4fe6ff006107f02883ff31
Robson Locatelli <rlocatelli9@gmail.com>
2023-11-04 08:54:08 -0300

    feat: created password hash

--- src/http/controllers/Users/register.ts
+++ src/http/controllers/Users/register.ts
@@ -1,4 +1,5 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
 import { prisma } from 'src/lib/prisma'
 import { z } from 'zod'
 
@@ -14,11 +15,23 @@ export default async function Register(
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const passwordHash = await hash(password, 6)
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userExists) {
+    return reply.status(409).send()
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash: passwordHash,
     },
   })
 
