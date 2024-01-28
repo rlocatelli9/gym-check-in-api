1e5664f2d480356cebb7de87080503dc5f1a2086
Robson Locatelli <rlocatelli9@gmail.com>
2024-01-28 17:28:40 -0300

    feat: created Profile controller and auth middleware

--- src/http/controllers/Users/profile.ts
+++ src/http/controllers/Users/profile.ts
@@ -1,12 +1,19 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeGetUserProfileService } from 'src/services/factories/make-get-user-profile-service'
 
 export default async function Profile(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileService()
 
-  console.log(request.user.sub)
+  const profile = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
 
-  return reply.status(200).send()
+  const user = Object.assign({}, { ...profile.user, password_hash: undefined })
+
+  return reply.status(200).send({
+    user,
+  })
 }
