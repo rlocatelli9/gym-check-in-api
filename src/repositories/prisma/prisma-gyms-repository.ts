d3653405bc6c4f2227a1daf8e457e789e817f052
Robson Locatelli <rlocatelli9@gmail.com>
2024-01-28 15:50:09 -0300

    feat: finished prisma gyms repository

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -1,6 +1,7 @@
 import { prisma } from 'src/lib/prisma'
-import { Prisma } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
 import { IGymsRepository } from '../interfaces'
+import { SearchNearbyGymServiceProps } from 'src/services/interfaces'
 
 export default class PrismaGymsRepository implements IGymsRepository {
   async create(data: Prisma.GymCreateInput) {
@@ -23,5 +24,43 @@ export default class PrismaGymsRepository implements IGymsRepository {
     }
 
     return gym
+  }
+
+  async findManyNearby({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  }: SearchNearbyGymServiceProps) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10 
+    `
+
+    return gyms
+  }
+
+  async searchManyByQuery(query: string, page: number) {
+    const querySearch = query.trim().split(';') ?? []
+    let fields = {}
+
+    if (querySearch.length > 0) {
+      querySearch
+        .map((element) => {
+          const [field, value] = element.split('=')
+          return { [field]: value }
+        })
+        .forEach((item) => {
+          fields = { ...fields, ...item }
+        })
+    }
+
+    const gyms = await prisma.gym.findMany({
+      where: {
+        ...fields,
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
   }
 }
