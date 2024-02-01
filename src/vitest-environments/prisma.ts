41b82bb2cc4e481681cbffe65f65abecb4bf1860
Robson Locatelli <rlocatelli9@gmail.com>
2024-02-01 18:17:59 -0300

    chore: created custom schema database for test environments

--- src/vitest-environments/prisma.ts
+++ src/vitest-environments/prisma.ts
@@ -1,13 +1,41 @@
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import type { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+// DATABASE_URL="postgresql://docker:docker@localhost:5432/api-gym-check?schema=public"
+const prismaClient = new PrismaClient()
+
+function generateURLDatabase(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL not specified')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   setup() {
-    // custom setup
+    const schema = randomUUID()
+    const databaseURL = generateURLDatabase(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
+
     return {
-      teardown() {
-        // called after all tests with this env have been run
+      async teardown() {
+        await prismaClient.$executeRawUnsafe(`
+          DROP SCHEMA IF EXISTS "${schema}" CASCADE;
+        `)
+        await prismaClient.$disconnect()
       },
     }
   },
