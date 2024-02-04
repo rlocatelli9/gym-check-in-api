65ea342a2c1bf56534b94e2c09f3495b837e4611
Robson Locatelli <rlocatelli9@gmail.com>
2024-02-04 20:20:43 -0300

    refactor: adjusted zod validation

--- src/http/controllers/Gyms/create.ts
+++ src/http/controllers/Gyms/create.ts
@@ -7,10 +7,10 @@ export async function CreateGym(request: FastifyRequest, reply: FastifyReply) {
     title: z.string(),
     description: z.string().nullable(),
     phone: z.string().nullable(),
-    latitude: z.coerce.number().refine((value) => {
+    latitude: z.number().refine((value) => {
       return Math.abs(value) <= 90
     }),
-    longitude: z.coerce.number().refine((value) => {
+    longitude: z.number().refine((value) => {
       return Math.abs(value) <= 180
     }),
   })
